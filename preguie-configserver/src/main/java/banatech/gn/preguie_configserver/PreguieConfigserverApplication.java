ff356cb17ee2cacccfa1fad6b90d595358032233
kadesory92 <sorybana92@gmail.Com>
2025-02-22 12:27:47 +0100

    Configuration de Cloud Config

---
+++ preguie-configserver/src/main/java/banatech/gn/preguie_configserver/PreguieConfigserverApplication.java
@@ -0,0 +1,13 @@
+package banatech.gn.preguie_configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreguieConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreguieConfigserverApplication.class, args);
+	}
+
+}
---
+++ preguie-configserver/src/test/java/banatech/gn/preguie_configserver/PreguieConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package banatech.gn.preguie_configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreguieConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
